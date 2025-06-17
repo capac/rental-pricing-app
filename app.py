2aea256791f6cc0e8125ce5d9f6d0a50eb9fff25
capac <capac@users.noreply.github.com>
2025-06-17 16:43:47 +0100

    Updated application with model from expanded dataset

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import re
-import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
+import streamlit as st
 
 
 @st.cache_resource
@@ -13,34 +12,48 @@ def load_model():
 model = load_model()
 
 st.title('Short-Term Rental Price Estimator')
+
 st.write(
     'Use this tool to estimate the short-term rental nightly '
     'price of your property based on its characteristics and '
     'location in London, UK.'
     )
+
 st.write(
     '**Short-term rentals are those where the nights stay is from '
-    '30 days to 999 days. The data set used for modeling is from '
+    '1 day to 999 days. The data set used for modeling is from '
     'December 2024.**'
 )
 
 property_type = st.selectbox(
     'Property type',
-    ['Entire rental unit', 'Entire townhouse', 'Entire condo',
-     'Entire serviced apartment', 'Entire guest suite',
-     'Entire loft', 'Entire home', 'Entire guesthouse',
-     'Room in aparthotel', 'Entire place', 'Camper/RV',
-     'Tiny home', 'Boat', 'Entire bungalow']
+    ['Entire rental unit', 'Entire condo', 'Private room in home',
+     'Private room in rental unit', 'Entire townhouse',
+     'Private room in townhouse', 'Private room in condo',
+     'Private room in bed and breakfast', 'Entire home',
+     'Entire guest suite', 'Private room in serviced apartment',
+     'Entire loft', 'Private room in guesthouse', 'Entire serviced apartment',
+     'Private room in loft', 'Private room in guest suite',
+     'Entire guesthouse', 'Room in hotel']
     )
+
+room_type = st.selectbox(
+    'Room type',
+    ['Entire home/apt', 'Private room'])
+
+accommodates = st.number_input(
+    'Number of people property can accommodate', min_value=0,
+    max_value=20, value=1, step=1
+    )
+
 bedrooms = st.number_input(
     'Number of bedrooms', min_value=0, max_value=20, value=1, step=1
     )
+
 bathrooms = st.number_input(
     'Number of bathrooms', min_value=0, max_value=20, value=1, step=1
     )
-minimum_nights = st.number_input(
-    'Minimum nights stay', min_value=30, max_value=999, value=30, step=1
-    )
+
 borough = st.selectbox(
     'London borough',
     ['Barking and Dagenham', 'Barnet', 'Bexley', 'Brent',
@@ -53,31 +66,43 @@ borough = st.selectbox(
      'Waltham Forest', 'Wandsworth', 'Westminster']
      )
 
-distance_to_station = st.slider(
-    'Distance to nearest station (km)',
-    min_value=0.0, max_value=20.0, value=0.5, step=0.1
+availability_365 = st.slider(
+    'Current yearly availabiliy',
+    min_value=0, max_value=365, value=0, step=1
     )
 
-amenity_1 = st.selectbox(
+days_from_last_review = st.slider(
+    'Days from last review (maximum 6 months)',
+    min_value=0, max_value=182, value=0, step=1
+    )
+
+distance_to_station = st.slider(
+    'Distance to nearest station (km)',
+    min_value=0.0, max_value=10.0, value=0.0, step=0.1
+    )
+
+first_amenity = st.selectbox(
     'First nearby amenity',
-    ['Grocery Store', 'Restaurant', 'Cafe', 'Nightlife', 'Retail',
-     'Fitness', 'Wellness', 'Entertainment', 'Cultural', 'Outdoor',
-     'Transport', 'Healthcare', 'Services', 'Organization',
-     'Education', 'Religion', 'Home Improvement']
+    ['Dining and Drinking', 'Community and Government', 'None',
+     'Health and Medicine', 'Arts and Entertainment', 'Retail',
+     'Business and Professional Services', 'Landmarks and Outdoors',
+     'Sports and Recreation', 'Travel and Transportation', 'Event']
     )
-amenity_2 = st.selectbox(
+
+second_amenity = st.selectbox(
     'Second nearby amenity',
-    ['Grocery Store', 'Restaurant', 'Cafe', 'Nightlife', 'Retail',
-     'Fitness', 'Wellness', 'Entertainment', 'Cultural', 'Outdoor',
-     'Transport', 'Healthcare', 'Services', 'Organization',
-     'Education', 'Religion', 'Home Improvement']
+    ['Dining and Drinking', 'Community and Government', 'None',
+     'Health and Medicine', 'Arts and Entertainment', 'Retail',
+     'Business and Professional Services', 'Landmarks and Outdoors',
+     'Sports and Recreation', 'Travel and Transportation', 'Event']
     )
-amenity_3 = st.selectbox(
+
+third_amenity = st.selectbox(
     'Third nearby amenity',
-    ['Grocery Store', 'Restaurant', 'Cafe', 'Nightlife', 'Retail',
-     'Fitness', 'Wellness', 'Entertainment', 'Cultural', 'Outdoor',
-     'Transport', 'Healthcare', 'Services', 'Organization',
-     'Education', 'Religion', 'Home Improvement']
+    ['Dining and Drinking', 'Community and Government', 'None',
+     'Health and Medicine', 'Arts and Entertainment', 'Retail',
+     'Business and Professional Services', 'Landmarks and Outdoors',
+     'Sports and Recreation', 'Travel and Transportation', 'Event']
     )
 
 crime_rate = {
@@ -97,16 +122,19 @@ crime_rate = {
 
 def prepare_features():
     input_dict = {
-        'borough': re.sub(r'\s', r'_', borough),
         'property_type': property_type,
+        'room_type': room_type,
         'bathrooms': bathrooms,
         'bedrooms': bedrooms,
-        'minimum_nights': minimum_nights,
+        'accommodates': accommodates,
+        'borough': borough,
         'crime_rate': crime_rate[borough],
-        'distance_to_station': distance_to_station,
-        'amenity_1': amenity_1,
-        'amenity_2': amenity_2,
-        'amenity_3': amenity_3
+        'distance_to_nearest_tube_station': distance_to_station,
+        'availability_365': availability_365,
+        'days_from_last_review': days_from_last_review,
+        'first_amenity': first_amenity,
+        'second_amenity': second_amenity,
+        'third_amenity': third_amenity
     }
     return pd.DataFrame([input_dict])
 
